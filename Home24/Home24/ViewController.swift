167772c05fb5d5e9d704295a841dbfdc0b94317b
Ghropade,Naveen (415) <naveen.ghorpade@daimler.com>
2018-07-27 14:28:41 +0200

    Home24: Basic project creation.

---
+++ Home24/Home24/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Home24
+//
+//  Created by Ghorpade, Naveen (415) on 27.07.18.
+//  Copyright © 2018 Ghorpade, Naveen (415). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
